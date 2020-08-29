5f1383ca5bf1d3e397c8194fda58500923575d14
Hourann <tdkgn@outlook.com>
2020-08-29 17:45:44 +0800

    [hourann] feat: Implement numberFormmater

--- src/exercise_01/numberFormatter.js
+++ src/exercise_01/numberFormatter.js
@@ -7,6 +7,6 @@ export default function formatNumber(number, option) {
   //
   // * Please implement the function and pass all the tests in format_number_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  const prefix = (option && option.currency) ? '$ ' : '';
+  return prefix + number.toFixed(2);
 }

cb1699587385ac0d15a097972a05e05883a5d4d0
Hourann <tdkgn@outlook.com>
2020-08-29 22:24:38 +0800

    [hourann] feat: Implement arrayToList

--- src/exercise_04/arrayToList.js
+++ src/exercise_04/arrayToList.js
@@ -5,6 +5,15 @@ export default function arrayToList(array) {
   //
   // * Please implement this function and pass all the tests in array_to_list_spec.js.
   // * Please do NOT modify the signature of the function.
-
-  throw new Error('Please delete this line and implement the function');
+  if (!Array.isArray(array)) {
+    throw new Error('Creating list from undefined array');
+  }
+  if (array.length === 0) {
+    throw new Error('Creating list from empty array');
+  }
+  const list = { head: null };
+  array.reverse().forEach((value) => {
+    list.head = { value, next: list.head };
+  });
+  return list.head;
 }

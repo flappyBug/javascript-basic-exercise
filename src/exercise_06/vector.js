7a393405e90d37c739da49f690acb511586baeeb
Hourann <tdkgn@outlook.com>
2020-08-29 22:51:39 +0800

    [hourann] feat: Implement Vector

--- src/exercise_06/vector.js
+++ src/exercise_06/vector.js
@@ -11,6 +11,27 @@ export default class Vector {
   // * Please implement the class and pass all the tests in vector_spec.js.
   // * Please do NOT modify the signature of the class. Please put all your code in the Vector
   // class.
+  constructor(x, y) {
+    Object.defineProperties(this, {
+      x: {
+        value: x,
+      },
+      y: {
+        value: y,
+      },
+    });
+  }
 
-  // PLEASE DELETE THIS LINE AND ADD YOUR IMPLEMENTATION HERE
+  static plus(v1, v2) {
+    return new Vector(v1.x + v2.x, v1.y + v2.y);
+  }
+
+  static minus(v1, v2) {
+    return new Vector(v1.x - v2.x, v1.y - v2.y);
+  }
+
+  distance() {
+    const { x, y } = this;
+    return Math.sqrt(x * x + y * y);
+  }
 }
